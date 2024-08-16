803cc77cbe7c8dfe7ff2c93ac260cd22363f0f20
CampusVirtualTunja <campusvirtualsolicitud@gmail.com>
2024-08-16 16:04:23 -0500

    favicon

---
+++ precache-manifest.7d1177386fc69fc925fba65a910158cb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a702fa69356f9f2e83a705922292283",
+    "url": "@yield('content-viewer-url')/index.html"
+  },
+  {
+    "revision": "0ce8bfabc1973b3e95b2",
+    "url": "@yield('content-viewer-url')/static/css/2.37be29a2.chunk.css"
+  },
+  {
+    "revision": "857ca5b07c245980e836",
+    "url": "@yield('content-viewer-url')/static/css/main.f6d68eb3.chunk.css"
+  },
+  {
+    "revision": "0ce8bfabc1973b3e95b2",
+    "url": "@yield('content-viewer-url')/static/js/2.3c58696b.chunk.js"
+  },
+  {
+    "revision": "cfb333affc1e9e4e40ea7a1e949500b6",
+    "url": "@yield('content-viewer-url')/static/js/2.3c58696b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "857ca5b07c245980e836",
+    "url": "@yield('content-viewer-url')/static/js/main.c1c5a2e9.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "@yield('content-viewer-url')/static/js/main.c1c5a2e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33a12fe361989d2d462",
+    "url": "@yield('content-viewer-url')/static/js/runtime-main.eac69593.js"
+  },
+  {
+    "revision": "a754c5ed00d8f6cca2a96794a2beaddc",
+    "url": "@yield('content-viewer-url')/static/media/trophy.a754c5ed.svg"
+  }
+]);
